b6fe813ac44505980bb5b25c5fe061c93a6b9faf
Virus_Mix <sashamoik2016@gmail.com>
2019-09-28 13:55:38 +0300

    oop lr2 start

---
+++ 3course OOP/CPP Labs/laba2/DateTime.h
@@ -0,0 +1,35 @@
+class DateTime
+{
+private:
+	unsigned short int day;
+	unsigned short int month;
+	unsigned int year;
+public:
+	DateTime();
+	DateTime(unsigned short int d, unsigned short int m, unsigned int y);
+	~DateTime();
+	
+	void setAll(unsigned short int d, unsigned short int m, unsigned int y);
+	
+	int getYear();
+	int getMounth();
+	int getDay();
+	
+	DateTime nextDay(DateTime);
+	DateTime previousDay(DateTime);
+	int countEndDays();
+	
+	DateTime inputDate();
+	void showDate();
+
+	bool operator&(DateTime);
+	bool operator!();
+	bool logic();
+	operator string() {
+		string result = "";
+
+
+
+		return result;
+	}
+};
